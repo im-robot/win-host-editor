6ce9ec50389099c670ef8c134b640134750a963c
im-robot <1511797414@qq.com>
2019-12-10 14:43:35 +0800

    :update:取消悬浮窗，增加托盘角标

--- app/index.js
+++ app/index.js
@@ -1,83 +1,97 @@
-const { app, BrowserWindow, ipcMain, Menu, dialog } = require('electron')
+const { app, BrowserWindow, ipcMain, Menu, dialog, Tray } = require('electron')
 const path = require('path')
+const os = require('os')
 const renderFile = path.resolve(__dirname, '../view/index.html')
-const suspendFile = path.resolve(__dirname, '../view/suspend.html')
 const icon = path.resolve(__dirname, '../icon.ico')
 const rt = require('is-root')
 
-var mainWin = null
-var renderWin = null
+let mainWin = null
+let tray = null
 let isRoot = rt()
+const isMac = os.platform() === 'darwin'
 
 
 
 const gotTheLock = app.requestSingleInstanceLock()
 
 if (!gotTheLock) {
-  app.quit()
+  quit()
 } else {
   app.on('second-instance', (event, commandLine, workingDirectory) => {
     if (mainWin) {
       if (mainWin.isMinimized()) mainWin.restore()
+      mainWin.show()
       mainWin.focus()
-      createRenderWindow()
     }
   })
   app.on('ready', createWindow)
 }
 
 function createWindow () {
-
   if (!isRoot) {
-    dialog.showErrorBox('错误警告','请使用管理员身份打开此' +
-        '应用!')
-    app.quit()
+    let w = '请使用管理员身份运行'
+    let m = '请使用root身份运行\nsudo '+ app.getPath("exe")+' ; exit ;'
+    dialog.showErrorBox('错误提示：', isMac ? m : w)
+    quit()
+    return false
   }
   // 创建浏览器窗口
   mainWin = new BrowserWindow({
-    width: 130,
-    height: 36,
-    frame: false,
-    transparent: true,
-    resizable: false,
-    minimizable: false,
-    maximizable: false,
-    closable: false,
-    alwaysOnTop: true,
+    icon,
+    width: 800,
+    height: 600,
     webPreferences: {
       nodeIntegration: true
     }
   })
-  mainWin.setSkipTaskbar(true)
-  // 加载index.html文件
-  mainWin.loadFile(suspendFile)
-}
 
-
-function createRenderWindow () {
-  if (renderWin) {
-    renderWin.show()
-    return false
-  } else {
-    renderWin = new BrowserWindow({
-      icon,
-      width: 800,
-      height: 600,
-      webPreferences: {
-        nodeIntegration: true
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: '退出',
+      click: () => {
+        quit()
       }
-    })
-    renderWin.setSkipTaskbar(true)
-    Menu.setApplicationMenu(null)
-    // 加载index.html文件
-    renderWin.loadFile(renderFile)
-    renderWin.on('close', (event) => { 
-      renderWin.hide()
-      event.preventDefault()
-    })
+    }
+  ])
+  tray = new Tray(path.join(__dirname, './h.png'))
+  tray.on('click', () => {
+    mainWin.isVisible() ? mainWin.hide() : mainWin.show()
+  })
+  tray.on('right-click', () => {
+    tray.popUpContextMenu(contextMenu)
+  })
+  tray.setToolTip('hosts编辑器')
+  // tray.setContextMenu(contextMenu)
+
+
+
+  if (isMac) {
+    app.dock.hide()
+  } else {
+    mainWin.setSkipTaskbar(true)
   }
-  
+  mainWin.on('show', () => {
+    tray.setImage(path.join(__dirname, './h.png'))
+  })
+  mainWin.on('hide', () => {
+    tray.setImage(path.join(__dirname, './h1.png'))
+  })
+  mainWin.on('close', (event) => {
+    mainWin.hide()
+    event.preventDefault()
+  })
+  // Menu.setApplicationMenu(null)
+  // 加载index.html文件
+  mainWin.loadFile(renderFile)
 }
-ipcMain.on('add', ()=>{
-  createRenderWindow()
-})
+
+function quit() {
+  if (tray.isDestroyed()) {
+    mainWin.destroy()
+    app.quit()
+  } else {
+    tray.destroy()
+    mainWin.destroy()
+    app.quit()
+  }
+}
